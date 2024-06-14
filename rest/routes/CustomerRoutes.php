16eb67e929228dd0d3d7dc531d29d38331f3a5fd
Eldar Gljiva <=eldar.gljiva@stu.ibu.edu.ba>
2024-06-14 16:29:46 +0200

    update application

--- rest/routes/CustomerRoutes.php
+++ rest/routes/CustomerRoutes.php
@@ -1,137 +1,24 @@
 <?php
 
 use \Firebase\JWT\JWT;
-use \Firebase\JWT\Key;
 
 require 'validateEmail.php';
 require 'validateMX.php';
 require 'validatePhone.php';
 
-// Route used to get all customers from db
-/**
- * @OA\Get(
- *      path="/customers",
- *      tags={"customers"},
- *      summary="Get all customers",
- *      security={
- *          {"ApiKey": {}}
- *      },
- *      @OA\Response(
- *           response=200,
- *           description="Get all customers"
- *      ),
- * )
- */
+
 Flight::route("GET /customers", function () {
     Flight::json(Flight::customerService()->getAll());
 });
 
-// Route used to get customer by id from db
-/**
- * @OA\Get(
- *      path="/customers/{id}",
- *      tags={"customers"},
- *      summary="Get a customer by id",
- *      security={
- *          {"ApiKey": {}}
- *      },
- *      @OA\Response(
- *           response=200,
- *           description="Get a customer by id"
- *      ),
- *   @OA\Parameter(@OA\Schema(type="number"), in="path", name="id", example="1", description="Patient ID"),
- * )
- */
 Flight::route("GET /customers/@id", function ($id) {
     Flight::json(Flight::customerService()->getById($id));
 });
 
-// Route used to delete customer by id from db
-/**
- * @OA\Delete(
- *      path="/customers/{id}",
- *      tags={"customers"},
- *      summary="Delete a customer by id",
- *      security={
- *          {"ApiKey": {}}
- *      },
- *      @OA\Response(
- *           response=200,
- *           description="Deleted Customer Data"
- *      ),
- *   @OA\Parameter(@OA\Schema(type="number"), in="path", name="id", example="1", description="Patient ID"),
- * )
- */
+
 Flight::route("DELETE /customers/@id", function ($id) {
     Flight::json(["message" => "Customer Deleted Succesfully", "data" => Flight::customerService()->delete($id)]);
 });
-
-// Route used to add customer to db
-/**
- * @OA\Post(
- *      path="/customers/register",
- *      tags={"customers"},
- *      summary="Register a customer",
- *      security={
- *          {"ApiKey": {}}
- *      },
- *      @OA\RequestBody(
- *          required=true,
- *          description="Customer data payload",
- *          @OA\JsonContent(
- *              type="object",
- *              @OA\Property(
- *                  property="fName",
- *                  type="string",
- *                  example="web",
- *                  description="Customer First Name"
- *              ),
- *              @OA\Property(
- *                  property="lName",
- *                  type="string",
- *                  example="programming",
- *                  description="Customer Last Name"
- *              ),
- *              @OA\Property(
- *                  property="email",
- *                  type="string",
- *                  example="web@gmail.com",
- *                  description="Customer Email"
- *              ),
- *              @OA\Property(
- *                  property="password",
- *                  type="string",
- *                  example="web123",
- *                  description="Customer Password"
- *              ),
- *              @OA\Property(
- *                  property="phone",
- *                  type="string",
- *                  example="123456789",
- *                  description="Customer Phone Number"
- *              ),
- *              @OA\Property(
- *                  property="role",
- *                  type="string",
- *                  example="customer",
- *                  description="Customer Role"
- *              )
- *          )
- *      ),
- *      @OA\Response(
- *           response=200,
- *           description="Customer registered successfully"
- *      ),
- *      @OA\Response(
- *           response=400,
- *           description="Invalid phone number, Invalid email TLD, Password found in the pwned database, Email already exists in the database, Phone is already registered"
- *      ),
- *      @OA\Response(
- *           response=500,
- *           description="Failed to register customer, cURL Error: <error message>"
- *      )
- * )
- */
 
 Flight::route("POST /customers/register", function () {
     $data = Flight::request()->data->getData();
@@ -206,45 +93,6 @@ Flight::route("POST /customers/register", function () {
         }
     }
 });
-
-/**
- * @OA\Post(
- *      path="/customers/login",
- *      tags={"customers"},
- *      summary="Customer login",
- *      @OA\RequestBody(
- *          required=true,
- *          description="Customer login credentials",
- *          @OA\JsonContent(
- *              type="object",
- *              @OA\Property(
- *                  property="email",
- *                  type="string",
- *                  example="web@gmail.com",
- *                  description="Customer Email"
- *              ),
- *              @OA\Property(
- *                  property="password",
- *                  type="string",
- *                  example="web123",
- *                  description="Customer Password"
- *              )
- *          )
- *      ),
- *      @OA\Response(
- *           response=200,
- *           description="Customer logged in successfully"
- *      ),
- *      @OA\Response(
- *           response=400,
- *           description="Email and password are required"
- *      ),
- *      @OA\Response(
- *           response=401,
- *           description="Invalid email or password"
- *      )
- * )
- */
 
 Flight::route("POST /customers/login", function () {
     $data = Flight::request()->data->getData();
